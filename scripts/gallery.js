37f7ab3d7ec9a697ae13f682ac77bfefc4dea8a3
Y02U1 <y02u1.lotr@gmail.com>
2018-03-26 20:28:12 +0200

    Appropriazione del codice del modale HTML/CSS/JS  :smiling_imp:

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,25 +1,24 @@
 window.addEventListener("load", function () {
-    // Get the modal
-    var modal = document.getElementById('myModal');
+    var modal = document.getElementById('galleryModal'); // Prendi il modale
+    var modalImg = document.getElementById("modal-img"); // Prendi l'immagine
+    var captionText = document.getElementById("caption"); // Prendi la didascalia
+    imgs = document.getElementsByClassName("hover-shadow"); // Prendi le immagini della galleria
 
-    // Get the image and insert it inside the modal - use its "alt" text as a caption
-    // var img = document.getElementById('myImg');
-    var modalImg = document.getElementById("img01");
-    var captionText = document.getElementById("caption");
-    imgs = document.getElementsByClassName("hover-shadow");
-
+    // Per ogni immagine...
     for (var i = 0; i < imgs.length; i++) {
+        // ...Crea la funzione...
         imgs[i].onclick = function(){
+            // ...Che apre il modale con i parametri dell'immagine
             modal.style.display = "block";
             modalImg.src = this.src;
             captionText.innerHTML = this.alt;
         }
     }
 
-    // Get the <span> element that closes the modal
+    // Prendi lo <span> (chiusura)
     var span = document.getElementsByClassName("close")[0];
 
-    // When the user clicks on <span> (x), close the modal
+    // Quando si clicca sullo <span> chiudi il modale
     span.onclick = function() {
         modal.style.display = "none";
     }
